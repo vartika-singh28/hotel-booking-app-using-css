8af6fd4e53d0bad903702175f78815c96720d270
Vartika Singh <104501858+vartika-singh28@users.noreply.github.com>
2022-09-01 17:28:36 +0530

    Add files via upload

---
+++ hotel.js
@@ -0,0 +1,21 @@
+const navBtn = document.querySelector('#nav-btn');
+const cancelBtn = document.querySelector('#cancel-btn');
+const sideNav = document.querySelector('#sidenav');
+const modal = document.querySelector('#modal');
+
+navBtn.addEventListener("click", function(){
+    sideNav.classList.add('show');
+    modal.classList.add('showModal');
+});
+
+cancelBtn.addEventListener('click', function(){
+    sideNav.classList.remove('show');
+    modal.classList.remove('showModal');
+});
+
+window.addEventListener('click', function(event){
+    if(event.target === modal){
+        sideNav.classList.remove('show');
+        modal.classList.remove('showModal');
+    }
+});
